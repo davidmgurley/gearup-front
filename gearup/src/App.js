3fc04755c8ea8bf7a9e88e9cda32b9f4163512ac
David Gurley <davidmgurley@gmail.com>
2018-06-11 13:45:56 -0600

    initial commit. react app with semantic ui and firebase auth

---
+++ gearup/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import firebase from 'firebase'
+require('firebase/auth')
+
+const config = {
+    apiKey: "AIzaSyBpiStXZPZ_WaWefQ_cIw27Ipk7oj2e_OI",
+    authDomain: "denver-gear-up.firebaseapp.com",
+    databaseURL: "https://denver-gear-up.firebaseio.com",
+    projectId: "denver-gear-up",
+    storageBucket: "denver-gear-up.appspot.com",
+    messagingSenderId: "982226317434"
+  }
+  firebase.initializeApp(config)
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Denver Gear Up</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
