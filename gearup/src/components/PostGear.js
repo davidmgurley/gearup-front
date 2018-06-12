1c38c8c429869ec869c97f932156e2043d5f90da
David Gurley <davidmgurley@gmail.com>
2018-06-12 15:47:05 -0600

    added form

--- gearup/src/components/PostGear.js
+++ gearup/src/components/PostGear.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
-import { Card, Icon, Image, Button } from 'semantic-ui-react'
+import { Checkbox, TextArea, Card, Icon, Image, Button, Modal, Form, Header, Dropdown } from 'semantic-ui-react'
 
+const gearCategories = [
+  {text:'Camp',
+   value:'Camp' ,
+   image: {avatar:true, src:'http://szzljy.com/images/camping/camping2.jpg'}
+  },
+  {text:'Climb',
+   value:'Climb' ,
+   image: {avatar:true, src:'https://cdn.shopify.com/s/files/1/1554/2851/files/guisdebooks_pop2_580x.jpg'}
+  },
+  {text:'Cycle',
+   value:'Cycle' ,
+   image: {avatar:true, src:'https://pbs.twimg.com/media/DaHaELEUMAU8LEW.jpg'}
+  },
+  {text:'Paddle',
+   value:'Paddle' ,
+   image: {avatar:true, src:'https://upload.wikimedia.org/wikipedia/commons/9/96/Kayak_Playboat_ManchesterNH.jpg'}
+  },
+  {text:'Snow',
+   value:'Hike' ,
+   image: {avatar:true, src:'https://upload.wikimedia.org/wikipedia/commons/5/5d/Aviano_snowboarder_2.JPG'}
+  },
+  {text:'Camp',
+   value:'Camp' ,
+   image: {avatar:true, src:'http://honolulumagazine-images.dashdigital.com/images/2017/07-17/hike-of-the-month-ehukai-trail.jpg'}
+  }
+]
 
 class PostGear extends Component {
   state = {
-    postedItems: []
+    postedItems: [],
+    modalNewGearOpen: false
   }
 
   componentDidMount() {
@@ -14,35 +41,53 @@ class PostGear extends Component {
       postedItems: response.gear,
     }))
     .then(response => console.log(this.state.postedItems))
-  } 
-  
+  }
+
+  handleNewGearOpen = (event) => this.setState({ modalNewGearOpen: true })
+  handleNewGearClose = (event) => this.setState({ modalNewGearOpen: false })
 
   render() {
     return (
       <div>
         <h1> User Profile </h1>
-        <Modal trigger={ <Button onClick={this.handleSignUpOpen} className='add-gear'>Add New Item</Button>} open={this.state.modalSignUpOpen}
-        onClose={this.handleSignUpClose} basic size='small'>
-          <Header Icon='add user' content='Sign Up' />
+        <Modal trigger={ <Button onClick={this.handleNewGearOpen} className='add-gear'>Add New Item</Button>} open={this.state.modalNewGearOpen}
+        onClose={this.handleNewGearClose} basic size='small'>
+          <Header Icon='add user' content='Add New Gear' />
           <Modal.Content>
-            <p>
-              Enter your email address and new password to create an account.
-            </p>
             <Form>
               <Form.Field>
-                <label>Email Address</label>
-                <input id='signup-email' placeholder='Email Address' />
+                <label>Image URL</label>
+                <input id='new-gear-image-url' placeholder='Image Url' />
               </Form.Field>
               <Form.Field>
-                <label>Password</label>
-                <input id='signup-password' type='password' placeholder='Password' />
+                <Dropdown id='new-gear-category' placeholder='Category' fluid selection options={ gearCategories } />
+              </Form.Field>
+              <Form.Field>
+                <label>Gear Type</label>
+                <input id='new-gear-gear-type' placeholder='Gear Type e.g. Tent' />
+              </Form.Field>
+              <Form.Field>
+                <label>Manufacturer</label>
+                <input id='new-gear-manufacturer' placeholder='Manufacturer' />
+              </Form.Field>
+              <Form.Field>
+                <label>Description</label>
+                <TextArea id='new-gear-description' placeholder='Description' maxLength='140' />
+              </Form.Field>
+              <Form.Field>
+                <label>Price per Day</label>
+                <input id='new-gear-price' placeholder='Price Per Day' />
+              </Form.Field>
+              <Form.Field>
+                <label>Availability</label>
+                <Checkbox toggle id='new-gear-availability' />
               </Form.Field>
               <Button onClick={(event) => { this.props.signUp(event); this.handleSignUpClose(event); setTimeout(function () { this.visibleButton(event) }.bind(this), 500) } }>Submit</Button>
           </Form>
           </Modal.Content>
           <Modal.Actions>
           </Modal.Actions>
-        </Modal>        
+        </Modal>
         <section>
           {this.state.postedItems.map((item,index) => {
             return <Card key={index}>
@@ -56,7 +101,7 @@ class PostGear extends Component {
               </Card.Content>
               <Button>Edit</Button>
             </Card>
-          })}        
+          })}
         </section>
       </div>
     )
