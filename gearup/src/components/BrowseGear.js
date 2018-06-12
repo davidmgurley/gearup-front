605e308f37da1eb710cc875a4d34c6179931d77c
Keith Decker <kdecker09@gmail.com>
2018-06-12 13:25:08 -0600

    selected items from category populating correctly

--- gearup/src/components/BrowseGear.js
+++ gearup/src/components/BrowseGear.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Icon, Image, Grid } from 'semantic-ui-react'
+import { Card, Icon, Image, Grid , Button} from 'semantic-ui-react'
 
 
 
@@ -9,24 +9,28 @@ class BrowseGear extends Component {
     this.state = {
       categories: [],
       showCategories: true,
-      showItems: false,
-      categorySelected: ''
+      categoryItems: []
     }
   }
 
   componentDidMount() {
     fetch('../gearCategories.json')
     .then(response => response.json())
-    .then(response => this.setState({categories: response}, () => {console.log(this.state.categories)}))
+    .then(response => this.setState({categories: response}))
 
 
-    fetch(`https://gear-up-backend.herokuapp.com/gear`)
-    .then(response => response.json())
-    // .then(response => console.log(response.gear))
+
   }
 
-  logInfo = (event) => {
-    console.log(event)
+  loaditems = (event) => {
+    // console.log(event)
+    fetch(`https://gear-up-backend.herokuapp.com/gear/category/${event}`)
+    .then(response => response.json())
+    .then(response => this.setState({
+      categoryItems: response.gear,
+      showCategories: false
+    }))
+    .then(response => console.log(this.state.categoryItems))
   }
 
   render() {
@@ -36,7 +40,7 @@ class BrowseGear extends Component {
           <section id='browse-container'>
 
             {this.state.categories.map((category, index) => {
-              return <Card key={index} onClick={(props) => this.logInfo(category.category.toLowerCase())}>
+              return <Card key={index} onClick={(props) => this.loadItems(category.category.toLowerCase())}>
                 <Image style={{height:'290px', width:'290px'}}src={category.image} />
                 <Card.Content>
                   <Card.Header style={{textAlign: 'center', fontSize: '200%', fontWeight: 'bold'}}>{category.category}</Card.Header>
@@ -44,7 +48,23 @@ class BrowseGear extends Component {
               </Card>
             })}
           </section>
-        : null }
+        :
+        <section>
+        {this.state.categoryItems.map((item,index) => {         
+           return <Card key={index}>
+              <Image style={{height:'290px', width:'290px'}} src={item.image_url} />
+              <Card.Content>
+                <Card.Header>{item.gear_type} - {item.manufacturer}</Card.Header>
+                <Card.Description>{item.description}</Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <Card.Header>${item.cost_per_day} per day - {item.available ? 'available' : 'unavailable'} </Card.Header>
+              </Card.Content>
+              <Button>Contact Now</Button>
+            </Card>            
+        })}
+        </section>
+       }
       </div>
     )
   }
